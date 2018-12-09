32218c66d46e5353b9bd65f9c44ce7fe040e9c20
Graham Wright <graham@opusaffair.com>
2018-12-09 12:36:55 -0500

    add travis yml

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,9 @@ import App, { Container } from "next/app";
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
-
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
-
     return { pageProps };
   }
 
